997764c687ebcffa355cd198c31bc632b1c54673
Kim Silva <joaquimsilvanetojunior@gmail.com>
2024-03-22 22:53:30 -0300

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/Header";
 
 
 const lato = Lato({
@@ -22,7 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={lato.className}>
-       <Header />
         {children}
       </body>
     </html>
